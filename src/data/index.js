99fc8f1441209e1fb36582dc48d11bfaa2c9df3e
Luis <ocasio.dev@gmail.com>
2020-12-01 03:32:10 +0000

    add price for items

--- src/data/index.js
+++ src/data/index.js
@@ -48,14 +48,14 @@ export const options = {
     { key: "pepper", text: "Pepper", value: "pepper" },
     { key: "ketchup", text: "Ketchup", value: "ketchup" },
   ],
-  LOADEDFRIES: [
+  "LOADED FRIES": [
     { key: "jalapenos", text: "Jalapenos", value: "jalapenos" },
     { key: "chilli", text: "Chilli", value: "chilli" },
     { key: "bacon", text: "Bacon", value: "bacon" },
     { key: "nacho cheese", text: "Nacho Cheese", value: "nacho cheese" },
     { key: "ranch", text: "Ranch", value: "ranch" },
   ],
-  CHEESESTEAK: [
+  "CHEESE STEAK": [
     { key: "bacon", text: "Bacon", value: "bacon" },
     { key: "cheese", text: "Cheese", value: "cheese" },
     { key: "onions", text: "Onions", value: "onions" },
@@ -107,14 +107,57 @@ export const exclude = {
     { key: "pepper", text: "Pepper", value: "pepper" },
     { key: "ketchup", text: "Ketchup", value: "ketchup" },
   ],
-  LOADEDFRIES: [
+  "LOADED FRIES": [
     { key: "bacon", text: "Bacon", value: "bacon" },
     { key: "nacho cheese", text: "Nacho Cheese", value: "nacho cheese" },
     { key: "ranch", text: "Ranch", value: "ranch" },
   ],
-  CHEESESTEAK: [
+  "CHEESE STEAK": [
     { key: "cheese", text: "Cheese", value: "cheese" },
     { key: "onions", text: "Onions", value: "onions" },
     { key: "mayo", text: "Mayo", value: "mayo" },
   ],
 };
+
+export const prices = [
+  {
+    id: 1,
+    name: "XL BURGER",
+    price: 5.99,
+  },
+  {
+    id: 2,
+    name: "XXL BURGER",
+    price: 7.99,
+  },
+  {
+    id: 3,
+    name: "GRILLED CHICKEN SANDWHICH",
+    price: 4.99,
+  },
+  {
+    id: 4,
+    name: "FRIED CHICKEN SANDWHICH",
+    price: 5.99,
+  },
+  {
+    id: 5,
+    name: "HOTDOG",
+    price: 2.99,
+  },
+  {
+    id: 6,
+    name: "FRIES",
+    price: 1.99,
+  },
+  {
+    id: 7,
+    name: "LOADED FRIES",
+    price: 3.99,
+  },
+  {
+    id: 8,
+    name: "CHEESE STEAK",
+    price: 6.99,
+  },
+];
